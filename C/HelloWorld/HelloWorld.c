29ac19f7ea45626b2268ff05ec9da4f0c3055c00
John Stockton <johnstocktoniv@gmail.com>
2022-01-11 11:26:59 -0500

    IO

--- C/HelloWorld/HelloWorld.c
+++ C/HelloWorld/HelloWorld.c
@@ -1,7 +1,27 @@
 #include<stdio.h>
 
-int main(void) {
-    printf("Hello, world");
+int main2(void) {
+    long s = 8245928625469605920l;
+    long p = 32183114504l;
+
+    while(p) {
+        putchar((s >> (((p >>= 3) & 7 ) << 3)) & 127 );
+    }
+    putchar('\n');
 
     return 0;
 }
+
+int main3() {
+    char str[20] = "Hello, World!";
+    printf("%s\n", str);
+    printf("Hello, World!\n");
+
+    return 0;
+}
+
+int main() {
+    main2();
+    main3();
+}
+
---
+++ C/IO/IO.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main( ) {
+
+   char str[100];
+   int i;
+
+   printf( "Enter a value: ");
+   scanf("%s", str);
+
+   printf( "\nYou entered: %s\n", str);
+
+   return 0;
+}
