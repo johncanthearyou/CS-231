2b016358bfaaaf668635bcb8a47a5127bb8a6dbb
John Stockton <johnstocktoniv@gmail.com>
2022-01-18 20:01:08 -0500

    Project1 V1.0

--- C/Project1/Project1.c
+++ C/Project1/Project1.c
@@ -1,72 +1,79 @@
 //John Stockton
-//CS-231
+//CS-231, Section 1
 //Dr. Vineyard
 //Winter 2022
 
 #include <stdio.h>
-#include <string.h>
 
 //Function to check if a given character is an uppercase letter
 //Inputs:  char, the character to be checked
-//Outputs: int, 0 if character is uppercase letter
-//              1 if character is NOT uppercase (including non-letters)
-int isUpper( char letter ) {
-    return (letter>='A' && letter<='Z');
-} 
+//Outputs: int (boolean), 0 if character is uppercase letter
+//                        1 if character is NOT uppercase letter
+int isUpper( char letter ) { return (letter>='A' && letter<='Z'); } 
 
 //Function to check if a given character is a lowercase letter
 //Inputs:  char, the character to be checked
-//Outputs: int, 0 if character is lowercase letter
-//              1 if character is NOT lowercase (including non-letters)
-int isLower( char letter ) {
-    return (letter>='a' && letter<='z');
-} 
+//Outputs: int (boolean), 0 if character is lowercase letter
+//                        1 if character is NOT lowercase letter
+int isLower( char letter ) { return (letter>='a' && letter<='z'); }
 
-//Function to compute the length of a given string (char[])
-//    by counting all the chars up to the '\0' char
-//Inputs:  char[], a string for which the length will be computed
-//Outputs: int, the length of the input string
-int length( char s[] ) {
-    int count = 0;
-    for( int i=0; s[i]!='\0'; i++ ) { count++; }
-    return count;
+//Function to compute the length of a given string by searching for '\0'
+//Inputs: pointer to char array, the for which we want to find length
+//Outputs: int, the length of the string
+int length( char *str ) {
+    int length = 0;
+    while( *str != '\0' ) {
+        length++;
+        str++;
+    }
+    return length;
 }
 
 //Function (in-place) to switch the case of all 
 //    constituent letters in a given string
 //Inputs:  char array, the string to be case-inverted
 //Outputs: pointer to char array, processed string  and case-inverted 
-char* switchCase( char str[] ) {
-    int strLength = length(str);
-
-    for( int i=0; i<strLength; i++ ) {
-        if( isUpper( str[i] ) ) { 
+char* switchCase( char *str ) {
+    while( *str != '\0' ) {
+        if( isUpper(*str) ) { 
             //letter is uppercase, modify ASCII value to lowercase letter    
-            str[i] += 32; 
-        } else if( isLower( str[i] ) ) { 
+            *str += 32; 
+        } else if( isLower(*str) ) { 
             //letter is lowercase, modify ASCII value to uppercase letter
-            str[i] -= 32; 
+            *str -= 32; 
         }
+        str++;
     }
-
-    return str;
+    return str; //Still the head of the string
 }
 
 int main( int argc, char * argv[] ) {
-    //Init input and output files
+    //Variable Declarations
     FILE *inputFile, *outputFile;
-    inputFile = fopen( argv[1], "r" ); //read only
-    outputFile = fopen( argv[2], "w"); //write only
-    
-    //TODO: Move on to new line for lines >=100
-    //define the maximum length of a line that can be read
     int maxLineSize = 101;
     char tmpLine[maxLineSize];
+    //Init input and output files
+    inputFile = fopen( argv[1], "r" ); //read only
+    if( inputFile==NULL ) {
+        printf( "ERROR! Unable to open the input file: %s\n", argv[1] );
+        return -1; //Generic fail condition
+    } else { outputFile = fopen( argv[2], "w"); } //write only
+
     while( fgets(tmpLine, maxLineSize, inputFile)!=NULL ) {
+        //Check if the current line is too long
+        if( length(tmpLine)==(maxLineSize-1) & tmpLine[maxLineSize-2]!='\n' ) {
+            if( fscanf(inputFile, "%*[^\n]")==0 ) {
+                printf( "%s\n", "Max length exceeded! The line will be truncated.");
+                
+            }
+        }
+        
         switchCase(tmpLine);
+        //Write the processed string to the output file
         fputs( tmpLine, outputFile );
     }
 
     fclose( inputFile );
     fclose( outputFile );
+    return 0;
 }
