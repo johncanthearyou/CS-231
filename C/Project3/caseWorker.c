7335344f0168264b72f10ab2f342adce7d863f06
John Stockton <johnstocktoniv@gmail.com>
2022-02-10 19:37:54 -0500

    Project3 caseWorker complete

--- C/Project3/caseWorker.c
+++ C/Project3/caseWorker.c
@@ -5,7 +5,12 @@
 
 #include <stdio.h>
 
-//TODO: Document...
+#define CASE_OFFSET 32
+
+//This function takes a character and determines if it is an uppercase vowel
+//Inputs: char, the character to check
+//OutputsL int (boolean), 1 if character is an uppercase vowel
+//                        0 if character is NOT an uppercase vowel
 int isVowelUpper( char letter ) {
     switch(letter) {
         case 'A':
@@ -18,31 +23,38 @@ int isVowelUpper( char letter ) {
     return 0;
 }
 
-//TODO: Document...
+//This function takes a character and determines if it is a lowercase consonant
+//Inputs: char, the character to check
+//OutputsL int (boolean), 1 if character is a lowercase consonant
+//                        0 if character is NOT a lowercase consonant
 int isConsonantLower( char letter ) {
     if( letter>='a' && letter<='z' ) {
+        //char is a lowercase letter
         switch(letter) {
             case 'a':
             case 'e':
             case 'i':
             case 'o':
             case 'u':
+                //we found a lowercase vowel, return false
                 return 0;
         }
+        //lowercase letter is not a vowel, it must be a consonant
         return 1;   
     }
     return 0;
 }
 
-//TODO: Document...
+//Main takes one argument, the name of the file to write to
+//    It will read words in from stdin, convert all vowels to lowercase
+//    and all consonants to uppercase, then print those words to the file given
 int main( int argc, char *argv[] ) {
     FILE *output = fopen( argv[1], "w" );
 
-    char tmpChar = fgetc(stdin);
-    while( tmpChar!=EOF ) {
-        if( isVowelUpper(tmpChar) ) { tmpChar += 32; }
-        else if( isConsonantLower(tmpChar) ) { tmpChar -= 32; }
+    char tmpChar;
+    while( (tmpChar=fgetc(stdin))!=EOF ) {
+        if( isVowelUpper(tmpChar) ) { tmpChar += CASE_OFFSET; }
+        else if( isConsonantLower(tmpChar) ) { tmpChar -= CASE_OFFSET; }
         fputc( tmpChar, output );
-        tmpChar = fgetc(stdin);
     }
 }
