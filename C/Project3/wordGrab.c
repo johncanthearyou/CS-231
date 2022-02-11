200a314c4f5c537ed03153506e9c9f8f376a10e2
John Stockton <johnstocktoniv@gmail.com>
2022-02-10 19:30:34 -0500

    Project3 wordGrab.c complete

--- C/Project3/wordGrab.c
+++ C/Project3/wordGrab.c
@@ -9,28 +9,30 @@
 #define WORD 1
 #define DELIMITER 2
 
-//TODO: Document...
+//This function takes a char and determines if it is a letter
+//Inputs: char, the letter to check
+//Outputs: int (boolean), 1 if given char is a letter
+//                        0 if given char is NOT a letter
 int isLetter( char letter ) {
     return ( (letter>='A' && letter<='Z') || (letter>='a' && letter<='z') );
 }
 
-//TODO: Document...
+//Main will take 1 argument, the name of the file from which to read
+//    It then parses the file for words reading one char at a time
+//    delimiting with any non-letter character, each of these parsed
+//    words is then printed to stdout
 int main( int argc, char *argv[] ) {
     FILE *input = fopen( argv[1], "r" );
     int state = INIT;
 
-    char tmpChar = fgetc(input);
-    while( tmpChar!=EOF ) {
-        switch( isLetter(tmpChar) ) {
-            case 1:
-                printf( "%c", tmpChar );
-                state = WORD;
-                break;
-            case 0:
-                if( state==WORD ) { printf( "\n" ); }
-                state = DELIMITER;
-                break;
+    char tmpChar;
+    while( (tmpChar=fgetc(input))!=EOF ) {
+        if( isLetter(tmpChar) ) {
+            state = WORD;
+            printf( "%c", tmpChar );
+        } else {
+            if( state==WORD ) { printf( "\n" ); }
+            state = DELIMITER;
         }
-        tmpChar = fgetc(input);
     }
 }
